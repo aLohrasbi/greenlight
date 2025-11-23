ece8aa4f1059ecc55757fd774443e40fb37f9a65
Arash Lohrasbi <arashlh@outlook.com>
2025-11-23 20:22:13 +0330

    postgres DB with pq

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -32,20 +32,20 @@ func ValidateMovie(v *validator.Validator, movie *Movie) {
 	// to true. For example, in the first line here we "check that the title is not
 	// equal to the empty string". In the second, we "check that the length of the title
 	// is less than or equal to 500 bytes" and so on.
-	v.Check(input.Title != "", "title", "must be provided")
-	v.Check(len(input.Title) <= 500, "title", "must not be more than 500 bytes long")
+	v.Check(movie.Title != "", "title", "must be provided")
+	v.Check(len(movie.Title) <= 500, "title", "must not be more than 500 bytes long")
 
-	v.Check(input.Year != 0, "year", "must be provided")
-	v.Check(input.Year >= 1888, "year", "must be greater than 1888")
-	v.Check(input.Year <= int32(time.Now().Year()), "year", "must not be in the future")
+	v.Check(movie.Year != 0, "year", "must be provided")
+	v.Check(movie.Year >= 1888, "year", "must be greater than 1888")
+	v.Check(movie.Year <= int32(time.Now().Year()), "year", "must not be in the future")
 
-	v.Check(input.Runtime != 0, "runtime", "must be provided")
-	v.Check(input.Runtime > 0, "runtime", "must be a positive integer")
+	v.Check(movie.Runtime != 0, "runtime", "must be provided")
+	v.Check(movie.Runtime > 0, "runtime", "must be a positive integer")
 
-	v.Check(input.Genres != nil, "genres", "must be provided")
-	v.Check(len(input.Genres) >= 1, "genres", "must contain at least 1 genre")
-	v.Check(len(input.Genres) <= 5, "genres", "must not contain more than 5 genres")
+	v.Check(movie.Genres != nil, "genres", "must be provided")
+	v.Check(len(movie.Genres) >= 1, "genres", "must contain at least 1 genre")
+	v.Check(len(movie.Genres) <= 5, "genres", "must not contain more than 5 genres")
 	// Note that we're using the Unique helper in the line below to check that all
 	// values in the input.Genres slice are unique.
-	v.Check(validator.Unique(input.Genres), "genres", "must not contain duplicate values")
+	v.Check(validator.Unique(movie.Genres), "genres", "must not contain duplicate values")
 }
