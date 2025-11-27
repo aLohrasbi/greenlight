98d5d72e126f21850af31972ac7d214928f70566
Arash Lohrasbi <arashlh@outlook.com>
2025-11-27 15:02:17 +0330

    IP-based Rate Limiting

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -1,36 +1,133 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
+	"sync"
+	"time"
+
+	"github.com/tomasen/realip"
+	"golang.org/x/time/rate"
 )
 
 func (app *application) recoverPanic(next http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        // Create a deferred function (which will always be run in the event of a panic
-        // as Go unwinds the stack).
-        defer func() {
-            // Use the builtin recover function to check if there has been a panic or
-            // not.
-            if err := recover(); err != nil {
-                // If there was a panic, set a "Connection: close" header on the 
-                // response. This acts as a trigger to make Go's HTTP server 
-                // automatically close the current connection after a response has been 
-                // sent.
-                w.Header().Set("Connection", "close")
-                // The value returned by recover() has the type any, so we use
-                // fmt.Errorf() to normalize it into an error and call our 
-                // serverErrorResponse() helper. In turn, this will log the error using
-                // our custom Logger type at the ERROR level and send the client a 500
-                // Internal Server Error response.
-                app.serverErrorResponse(w, r, fmt.Errorf("%s", err))
-            }
-        }()
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Create a deferred function (which will always be run in the event of a panic
+		// as Go unwinds the stack).
+		defer func() {
+			// Use the builtin recover function to check if there has been a panic or
+			// not.
+			if err := recover(); err != nil {
+				// If there was a panic, set a "Connection: close" header on the
+				// response. This acts as a trigger to make Go's HTTP server
+				// automatically close the current connection after a response has been
+				// sent.
+				w.Header().Set("Connection", "close")
+				// The value returned by recover() has the type any, so we use
+				// fmt.Errorf() to normalize it into an error and call our
+				// serverErrorResponse() helper. In turn, this will log the error using
+				// our custom Logger type at the ERROR level and send the client a 500
+				// Internal Server Error response.
+				app.serverErrorResponse(w, r, fmt.Errorf("%s", err))
+			}
+		}()
 
-        next.ServeHTTP(w, r)
+		next.ServeHTTP(w, r)
 		// Sets up a defer to catch panics.
 		// Then calls next.ServeHTTP(w, r) to run the actual handler.
 		// If that handler panics, the deferred function recovers and sends a 500 response.
 
-    })
+	})
+}
+func (app *application) rateLimit(next http.Handler) http.Handler {
+	// Global Rate Limit:
+	// Initialize a new rate limiter which allows an average of 2 requests per second,
+	// with a maximum of 4 requests in a single ‘burst’.
+	// limiter := rate.NewLimiter(2, 4)
+
+	// The function we are returning is a closure, which 'closes over' the limiter
+	// variable.
+	// return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	// Call limiter.Allow() to see if the request is permitted, and if it's not,
+	// then we call the rateLimitExceededResponse() helper to return a 429 Too Many
+	// Requests response (we will create this helper in a minute).
+	// 	if !limiter.Allow() {
+	// 		app.rateLimitExceededResponse(w, r)
+	// 		return
+	// 	}
+
+	// 	next.ServeHTTP(w, r)
+	// })
+	// IP based Rate Limit:
+	// Define a client struct to hold the rate limiter and last seen time for each
+	// client.
+	type client struct {
+		limiter  *rate.Limiter
+		lastSeen time.Time
+	}
+	// Declare a mutex and a map to hold the clients' IP addresses and rate limiters.
+	var (
+		mu sync.Mutex
+		// Update the map so the values are pointers to a client struct.
+		clients = make(map[string]*client)
+		// clients = make(map[string]*rate.Limiter)
+	)
+	// Launch a background goroutine which removes old entries from the clients map once
+	// every minute.
+	go func() {
+		for {
+			time.Sleep(time.Minute)
+
+			// Lock the mutex to prevent any rate limiter checks from happening while
+			// the cleanup is taking place.
+			mu.Lock()
+
+			// Loop through all clients. If they haven't been seen within the last three
+			// minutes, delete the corresponding entry from the map.
+			for ip, client := range clients {
+				if time.Since(client.lastSeen) > 3*time.Minute {
+					delete(clients, ip)
+				}
+			}
+
+			// Importantly, unlock the mutex when the cleanup is complete.
+			mu.Unlock()
+		}
+	}()
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Only carry out the check if rate limiting is enabled.
+		if app.config.limiter.enabled {
+			// Use the realip.FromRequest() function to get the client's IP address.
+			ip := realip.FromRequest(r)
+
+			// Lock the mutex to prevent this code from being executed concurrently.
+			mu.Lock()
+
+			// Check to see if the IP address already exists in the map. If it doesn't, then
+			// initialize a new rate limiter and add the IP address and limiter to the map.
+			if _, found := clients[ip]; !found {
+				// Create and add a new client struct to the map if it doesn't already exist.
+				clients[ip] = &client{limiter: rate.NewLimiter(2, 4)}
+			}
+
+			// Update the last seen time for the client.
+			clients[ip].lastSeen = time.Now()
+			// Call the Allow() method on the rate limiter for the current IP address. If
+			// the request isn't allowed, unlock the mutex and send a 429 Too Many Requests
+			// response, just like before.
+			if !clients[ip].limiter.Allow() {
+				mu.Unlock()
+				app.rateLimitExceededResponse(w, r)
+				return
+			}
+
+			// Very importantly, unlock the mutex before calling the next handler in the
+			// chain. Notice that we DON'T use defer to unlock the mutex, as that would mean
+			// that the mutex isn't unlocked until all the handlers downstream of this
+			// middleware have also returned.
+			mu.Unlock()
+		}
+		next.ServeHTTP(w, r)
+	})
 }
