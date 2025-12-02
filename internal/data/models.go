007bc9b34e0672b45e02b038c8d87d5080a55cf6
Arash Lohrasbi <arashlh@outlook.com>
2025-12-02 13:45:56 +0330

    Permission-based Authorization

--- internal/data/models.go
+++ internal/data/models.go
@@ -18,6 +18,7 @@ type Models struct {
 	Movies MovieModel
 	Users  UserModel
 	Tokens TokenModel
+	Permissions PermissionModel
 }
 
 // For ease of use, we also add a New() method which returns a Models struct containing
@@ -25,7 +26,8 @@ type Models struct {
 func NewModels(db *sql.DB) Models {
 	return Models{
 		Movies: MovieModel{DB: db},
-		Tokens: TokenModel{DB: db}, // Initialize a new TokenModel instance.
+		Tokens: TokenModel{DB: db}, 
 		Users:  UserModel{DB: db},
+		Permissions: PermissionModel{DB: db},
 	}
 }
