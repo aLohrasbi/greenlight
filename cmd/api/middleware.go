04b2e0b0852f94c030abda55ee4d90f0355b1db1
Arash Lohrasbi <arashlh@outlook.com>
2025-12-03 18:31:04 +0330

    Metrics with expvar (dev)

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"errors"
+	"expvar"
 	"fmt"
 	"net/http"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
@@ -263,78 +265,160 @@ func (app *application) requireActivatedUser(next http.HandlerFunc) http.Handler
 	// Wrap fn with the requireAuthenticatedUser() middleware before returning it.
 	return app.requireAuthenticatedUser(fn)
 }
+
 // Note that the first parameter for the middleware function is the permission code that
 // we require the user to have.
 func (app *application) requirePermission(code string, next http.HandlerFunc) http.HandlerFunc {
-    fn := func(w http.ResponseWriter, r *http.Request) {
-        // Retrieve the user from the request context.
-        user := app.contextGetUser(r)
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		// Retrieve the user from the request context.
+		user := app.contextGetUser(r)
 
-        // Get the slice of permissions for the user.
-        permissions, err := app.models.Permissions.GetAllForUser(user.ID)
-        if err != nil {
-            app.serverErrorResponse(w, r, err)
-            return
-        }
+		// Get the slice of permissions for the user.
+		permissions, err := app.models.Permissions.GetAllForUser(user.ID)
+		if err != nil {
+			app.serverErrorResponse(w, r, err)
+			return
+		}
 
-        // Check if the slice includes the required permission. If it doesn't, then 
-        // return a 403 Forbidden response.
-        if !permissions.Include(code) {
-            app.notPermittedResponse(w, r)
-            return
-        }
+		// Check if the slice includes the required permission. If it doesn't, then
+		// return a 403 Forbidden response.
+		if !permissions.Include(code) {
+			app.notPermittedResponse(w, r)
+			return
+		}
 
-        // Otherwise they have the required permission so we call the next handler in
-        // the chain.
-        next.ServeHTTP(w, r)
-    }
+		// Otherwise they have the required permission so we call the next handler in
+		// the chain.
+		next.ServeHTTP(w, r)
+	}
 
-    // Wrap this with the requireActivatedUser() middleware before returning it.
-    return app.requireActivatedUser(fn)
+	// Wrap this with the requireActivatedUser() middleware before returning it.
+	return app.requireActivatedUser(fn)
 }
 func (app *application) enableCORS(next http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        // Add the "Vary: Origin" header.
-        w.Header().Add("Vary", "Origin")
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Add the "Vary: Origin" header.
+		w.Header().Add("Vary", "Origin")
 
 		// Add the "Vary: Access-Control-Request-Method" header.
-        w.Header().Add("Vary", "Access-Control-Request-Method")
+		w.Header().Add("Vary", "Access-Control-Request-Method")
 
-        // Get the value of the request's Origin header.
-        origin := r.Header.Get("Origin")
+		// Get the value of the request's Origin header.
+		origin := r.Header.Get("Origin")
 
-        // Only run this if there's an Origin request header present.
-        if origin != "" {
-            // Loop through the list of trusted origins, checking to see if the request
-            // origin exactly matches one of them. If there are no trusted origins, then 
-            // the loop won't be iterated.
-            for i := range app.config.cors.trustedOrigins {
-                if origin == app.config.cors.trustedOrigins[i] {
-                    // If there is a match, then set a "Access-Control-Allow-Origin"
-                    // response header with the request origin as the value and break 
-                    // out of the loop.
-                    w.Header().Set("Access-Control-Allow-Origin", origin)
+		// Only run this if there's an Origin request header present.
+		if origin != "" {
+			// Loop through the list of trusted origins, checking to see if the request
+			// origin exactly matches one of them. If there are no trusted origins, then
+			// the loop won't be iterated.
+			for i := range app.config.cors.trustedOrigins {
+				if origin == app.config.cors.trustedOrigins[i] {
+					// If there is a match, then set a "Access-Control-Allow-Origin"
+					// response header with the request origin as the value and break
+					// out of the loop.
+					w.Header().Set("Access-Control-Allow-Origin", origin)
 
-					 // Check if the request has the HTTP method OPTIONS and contains the
-                    // "Access-Control-Request-Method" header. If it does, then we treat
-                    // it as a preflight request.
-                    if r.Method == http.MethodOptions && r.Header.Get("Access-Control-Request-Method") != "" {
-                        // Set the necessary preflight response headers, as discussed
-                        // previously.
-                        w.Header().Set("Access-Control-Allow-Methods", "OPTIONS, PUT, PATCH, DELETE")
-                        w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
+					// Check if the request has the HTTP method OPTIONS and contains the
+					// "Access-Control-Request-Method" header. If it does, then we treat
+					// it as a preflight request.
+					if r.Method == http.MethodOptions && r.Header.Get("Access-Control-Request-Method") != "" {
+						// Set the necessary preflight response headers, as discussed
+						// previously.
+						w.Header().Set("Access-Control-Allow-Methods", "OPTIONS, PUT, PATCH, DELETE")
+						w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
 
-                        // Write the headers along with a 200 OK status and return from
-                        // the middleware with no further action.
-                        w.WriteHeader(http.StatusOK)
-                        return
-                    }
-                    break
-                }
-            }
-        }
+						// Write the headers along with a 200 OK status and return from
+						// the middleware with no further action.
+						w.WriteHeader(http.StatusOK)
+						return
+					}
+					break
+				}
+			}
+		}
 
-        // Call the next handler in the chain.
-        next.ServeHTTP(w, r)
-    })
+		// Call the next handler in the chain.
+		next.ServeHTTP(w, r)
+	})
+}
+
+type metricsResponseWriter struct {
+	wrapped       http.ResponseWriter
+	statusCode    int
+	headerWritten bool
+}
+
+func newMetricsResponseWriter(w http.ResponseWriter) *metricsResponseWriter {
+	return &metricsResponseWriter{
+		wrapped:    w,
+		statusCode: http.StatusOK,
+	}
+}
+
+func (mw *metricsResponseWriter) Header() http.Header {
+	return mw.wrapped.Header()
+}
+
+func (mw *metricsResponseWriter) WriteHeader(statusCode int) {
+	mw.wrapped.WriteHeader(statusCode)
+
+	if !mw.headerWritten {
+		mw.statusCode = statusCode
+		mw.headerWritten = true
+	}
+}
+
+func (mw *metricsResponseWriter) Write(b []byte) (int, error) {
+	mw.headerWritten = true
+	return mw.wrapped.Write(b)
+}
+
+func (mw *metricsResponseWriter) Unwrap() http.ResponseWriter {
+	return mw.wrapped
+}
+func (app *application) metrics(next http.Handler) http.Handler {
+	// Initialize the new expvar variables when the middleware chain is first built.
+	var (
+		totalRequestsReceived           = expvar.NewInt("total_requests_received")
+		totalResponsesSent              = expvar.NewInt("total_responses_sent")
+		totalProcessingTimeMicroseconds = expvar.NewInt("total_processing_time_μs")
+
+		// Declare a new expvar map to hold the count of responses for each HTTP status
+		// code.
+		totalResponsesSentByStatus = expvar.NewMap("total_responses_sent_by_status")
+	)
+
+	// The following code will be run for every request...
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Record the time that we started to process the request.
+		start := time.Now()
+
+		// Use the Add() method to increment the number of requests received by 1.
+		totalRequestsReceived.Add(1)
+
+		// Create a new metricsResponseWriter, which wraps the original
+		// http.ResponseWriter value that the metrics middleware received.
+		mw := newMetricsResponseWriter(w)
+
+		// Call the next handler in the chain using the new metricsResponseWriter
+		// as the http.ResponseWriter value.
+		next.ServeHTTP(mw, r)
+
+		// On the way back up the middleware chain, increment the number of responses
+		// sent by 1.
+		totalResponsesSent.Add(1)
+
+		// At this point, the response status code should be stored in the
+		// mw.statusCode field. Note that the expvar map is string-keyed, so we
+		// need to use the strconv.Itoa() function to convert the status code
+		// (which is an integer) to a string. Then we use the Add() method on
+		// our new totalResponsesSentByStatus map to increment the count for the
+		// given status code by 1.
+		totalResponsesSentByStatus.Add(strconv.Itoa(mw.statusCode), 1)
+
+		// Calculate the number of microseconds since we began to process the request,
+		// then increment the total processing time by this amount.
+		duration := time.Since(start).Microseconds()
+		totalProcessingTimeMicroseconds.Add(duration)
+	})
 }
